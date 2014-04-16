7735c5c0f63a05a4a9946074e8fe2e7d536d8c4b
gsempe <guillaume.sempe@gmail.com>
2014-04-16 23:37:51 +0200

    First version of the boxing

---
+++ boxing.go
@@ -0,0 +1,267 @@
+// Package nanboxing implements methods to box and unbox stuff in NaN.
+//
+// Like it is done in Javascript languages to save variables with their type,
+// it can be useful to save different things in the NaN scope of float64 type
+package nanboxing
+
+import (
+	"fmt"
+	"math"
+	"unsafe"
+)
+
+// Box is the nanboxing top level type
+type Box float64
+
+// Object
+type Object map[string]Box
+
+// Array
+type Array []Box
+
+// tag defines the Box types
+type Tag uint64
+
+const (
+	TagNumber Tag = 0x1
+	tagString Tag = 0x2
+	TagBool   Tag = 0x3
+	TagArray  Tag = 0x4
+	TagObject Tag = 0x5
+	TagNull   Tag = 0xF
+)
+
+/*
+ payload width is enough to store :
+ - bool
+ - int32
+ - pointer (http://en.wikipedia.org/wiki/X86-64#Virtual_address_space_details)
+   - to array
+   - to object
+*/
+const (
+	TagMask     uint64 = 0xF
+	NaNMask     uint64 = 0x7FF8000000000000
+	PayloadMask uint64 = 0x00007FFFFFFFFFFF
+)
+
+const (
+	TagShift uint64 = 47
+)
+
+/*
+ String prints a Box
+*/
+func (x Box) String() string {
+	return fmt.Sprintf("Box is %X, address is %v\n", *(*uint64)(unsafe.Pointer(&x)), (*uint64)(unsafe.Pointer(&x)))
+}
+
+/*
+ Tag retrieve the tag of a box
+*/
+func (x Box) Tag() Tag {
+	return Tag(((*(*uint64)(unsafe.Pointer(&x))) >> TagShift) & TagMask)
+}
+
+/*
+ SetTag change the tag of a box
+*/
+// func (x Box) SetTag(t Tag) Box {
+// 	ui64 := NaNMask | (uint64(t) << TagShift) | (x.Payload() & PayloadMask)
+// 	return *(*Box)(unsafe.Pointer(&ui64))
+// }
+func (x *Box) SetTag(t Tag) {
+	ui64 := (NaNMask | (uint64(t) << TagShift) | (x.Payload() & PayloadMask))
+	*x = *(*Box)(unsafe.Pointer(&ui64))
+}
+
+/*
+ Payload retrieve the payload of a box
+*/
+func (x Box) Payload() uint64 {
+	return (*(*uint64)(unsafe.Pointer(&x))) & PayloadMask
+}
+
+func (x Box) SetPayload(u uint64) Box {
+	ui64 := NaNMask | (uint64(x.Tag()) << TagShift) | (u & PayloadMask)
+	return *(*Box)(unsafe.Pointer(&ui64))
+}
+
+/*
+ NewFloat create a float box
+*/
+func NewFloat(f float64) Box {
+	return Box(f)
+}
+
+/*
+ ToFloat retrieve the float value of a box
+*/
+func (x Box) ToFloat() float64 {
+	return float64(x)
+}
+
+/*
+ IsFloat64 test if a box type is float
+*/
+func (x Box) IsFloat64() bool {
+	return !math.IsNaN(float64(x))
+}
+
+/*
+ NewNumber create a int32 box
+*/
+func NewNumber(n int32) Box {
+	ui := *(*uint32)(unsafe.Pointer(&n))
+	ui64 := NaNMask | (uint64(TagNumber) << TagShift) | (uint64(ui) & PayloadMask)
+	return *(*Box)(unsafe.Pointer(&ui64))
+}
+
+/*
+ ToNumber retrieve the int32 value of a box
+*/
+func (x Box) ToNumber() int32 {
+	return int32(x.Payload())
+}
+
+/*
+ IsNumber test if a box type is number
+*/
+func (x Box) IsNumber() bool {
+	return !x.IsFloat64() && (x.Tag() == TagNumber)
+}
+
+/*
+  NewString create a string box
+
+ The string pointer is saved. String memory has to be keep by the box caller
+*/
+func NewString(s string) Box {
+	p := uintptr(unsafe.Pointer(&s))
+	ui64 := NaNMask | (uint64(tagString) << TagShift) | (uint64(p) & PayloadMask)
+	return *(*Box)(unsafe.Pointer(&ui64))
+}
+
+/*
+ ToString retrieve the string pointer of a box
+*/
+func (x Box) ToString() string {
+	return *(*string)(unsafe.Pointer(uintptr(x.Payload())))
+}
+
+/*
+ IsString Test if a box type is String
+*/
+func (x Box) IsString() bool {
+	return !x.IsFloat64() && (x.Tag() == tagString)
+}
+
+/*
+ NewBool create a bool box
+*/
+func NewBool(b bool) Box {
+	// fmt.Printf("ui 1 is %X address is %v\n", b, &b)
+	ui := *(*uint8)(unsafe.Pointer(&b))
+	// fmt.Printf("ui 2 is %X address is %v\n", ui, &ui)
+	ui64 := NaNMask | (uint64(TagBool) << TagShift) | (uint64(ui) & PayloadMask)
+	// fmt.Printf("ui 3 is %X address is %v\n", ui64, &ui64)
+	return *(*Box)(unsafe.Pointer(&ui64))
+}
+
+/*
+ ToBool retrieve the bool value of a box
+*/
+func (x Box) ToBool() bool {
+	return !(0 == x.Payload())
+}
+
+/*
+ IsBool test if a box type is bool
+*/
+func (x Box) IsBool() bool {
+
+	return !x.IsFloat64() && (x.Tag() == TagBool)
+}
+
+/*
+ NewArray create an array box
+
+ The array is saved as a pointer to a slice
+*/
+func NewArray(a Array) Box {
+	p := uintptr(unsafe.Pointer(&a))
+	ui64 := NaNMask | (uint64(TagArray) << TagShift) | (uint64(p) & PayloadMask)
+	return *(*Box)(unsafe.Pointer(&ui64))
+}
+
+/*
+ ToArray retrieve the array pointer of a box
+*/
+func (x Box) ToArray() Array {
+	return *(*Array)(unsafe.Pointer(uintptr(x.Payload())))
+}
+
+/*
+ IsArray test if a box type is array
+*/
+func (x Box) IsArray() bool {
+	return !x.IsFloat64() && (x.Tag() == TagArray)
+}
+
+/*
+ NewObject create an object box
+
+ The object is saved as a pointer to a map
+*/
+func NewObject(o Object) Box {
+	p := uintptr(unsafe.Pointer(&o))
+	ui64 := NaNMask | (uint64(TagObject) << TagShift) | (uint64(p) & PayloadMask)
+	return *(*Box)(unsafe.Pointer(&ui64))
+}
+
+/*
+ ToObject retrieve the object pointer of a box
+*/
+func (x Box) ToObject() Object {
+	return *(*Object)(unsafe.Pointer(uintptr(x.Payload())))
+}
+
+/*
+ IsObject tests if a box type is object
+*/
+func (x Box) IsObject() bool {
+	return !x.IsFloat64() && (x.Tag() == TagObject)
+}
+
+/*
+ NewNull create a null box
+*/
+func NewNull() Box {
+	ui64 := NaNMask | (uint64(TagNull) << TagShift) | (0 & PayloadMask)
+	return *(*Box)(unsafe.Pointer(&ui64))
+}
+
+/*
+ ToNull return nil, always nil
+*/
+func (x Box) ToNull() unsafe.Pointer {
+	return nil
+}
+
+/*
+ IsNull tests if a box is null
+*/
+func (x Box) IsNull() bool {
+	return !x.IsFloat64() && (x.Tag() == TagNull)
+}
+
+/*
+ IsPointer tests if a box is a pointer
+*/
+func (x Box) IsPointer() bool {
+	return !x.IsFloat64() && ((x.Tag() == TagArray) || (x.Tag() == TagObject))
+}
+
+func (x Box) ToPointer() uintptr {
+	return uintptr(x.Payload())
+}
---
+++ boxing_test.go
@@ -0,0 +1,176 @@
+package nanboxing
+
+import (
+	"math"
+	"testing"
+)
+
+func TestBoolBox(t *testing.T) {
+	trueBox := NewBool(true)
+	falseBox := NewBool(false)
+	if false == math.IsNaN(float64(trueBox)) {
+		t.Error("The box should be NaN")
+	}
+	if false == math.IsNaN(float64(falseBox)) {
+		t.Error("The box should be NaN")
+	}
+	if true != trueBox.ToBool() {
+		t.Error("trueBox value should be true")
+	}
+	if false != falseBox.ToBool() {
+		t.Error("falseBox value should be false")
+	}
+}
+
+func TestNumberBox(t *testing.T) {
+	{
+		zeroBox := NewNumber(0)
+		if false == math.IsNaN(float64(zeroBox)) {
+			t.Error("The box should be NaN")
+		}
+		if 0 != zeroBox.ToNumber() {
+			t.Error("zeroBox value should be 0")
+		}
+	}
+	{
+		negativeBox := NewNumber(-1)
+		if false == math.IsNaN(float64(negativeBox)) {
+			t.Error("The box should be NaN")
+		}
+		if -1 != negativeBox.ToNumber() {
+			t.Error("negativeBox value should be -1")
+		}
+	}
+	{
+		positiveBox := NewNumber(1)
+		if false == math.IsNaN(float64(positiveBox)) {
+			t.Error("The box should be NaN")
+		}
+		if 1 != positiveBox.ToNumber() {
+			t.Error("positiveBox value should be 1")
+		}
+	}
+	{
+		maxNegativeBox := NewNumber(math.MinInt32)
+		if false == math.IsNaN(float64(maxNegativeBox)) {
+			t.Error("The box should be NaN")
+		}
+		if math.MinInt32 != maxNegativeBox.ToNumber() {
+			t.Errorf("maxNegativeBox value should be %d", math.MinInt32)
+		}
+	}
+	{
+		maxPositiveBox := NewNumber(math.MaxInt32)
+		if false == math.IsNaN(float64(maxPositiveBox)) {
+			t.Error("The box should be NaN")
+		}
+		if math.MaxInt32 != maxPositiveBox.ToNumber() {
+			t.Errorf("maxPositiveBox value should be %d", math.MaxInt32)
+		}
+	}
+}
+
+func TestObject(t *testing.T) {
+	objectSource := make(Object)
+	objectSource["key1"] = NewNumber(1)
+	objectSource["key2"] = NewNumber(2)
+
+	objectBox := NewObject(objectSource)
+	if false == math.IsNaN(float64(objectBox)) {
+		t.Error("The box should be NaN")
+	}
+	objectTarget := objectBox.ToObject()
+	if len(objectSource) != len(objectTarget) {
+		t.Error("objectTarget and objectSource should have the same length")
+	}
+	if 1 != objectTarget["key1"].ToNumber() {
+		t.Error("objectTarget[\"key1\"] should be equal to objectSource[\"key1\"")
+	}
+	if 2 != objectTarget["key2"].ToNumber() {
+		t.Error("objectTarget[\"key2\"] should be equal to objectSource[\"key2\"")
+	}
+}
+
+func TestArray(t *testing.T) {
+	arraySource := Array{NewNumber(1), NewNumber(2)}
+	arrayBox := NewArray(arraySource)
+	if false == math.IsNaN(float64(arrayBox)) {
+		t.Error("The box should be NaN")
+	}
+	arrayTarget := arrayBox.ToArray()
+	numberBox1 := arrayTarget[0]
+	numberBox2 := arrayTarget[1]
+	if 1 != numberBox1.ToNumber() {
+		t.Error("arrayTarget[0] should be equal to arraySource[0]")
+	}
+	if 2 != numberBox2.ToNumber() {
+		t.Error("arrayTarget[1] should be equal to arraySource[1]")
+	}
+}
+func TestTag(t *testing.T) {
+	box := NewBool(true)
+	tag := box.Tag()
+	if TagBool != tag {
+		t.Errorf("The tag should be Bool, box value %x, NaN value %x, test %x", box, math.NaN(), uint64(0x7FF8000000000001))
+	}
+	maxPositiveBox := NewNumber(math.MaxInt32)
+	tag = maxPositiveBox.Tag()
+	if TagNumber != tag {
+		t.Errorf("The tag should be Number, box value %x.")
+	}
+	(&maxPositiveBox).SetTag(tagString)
+	tag = maxPositiveBox.Tag()
+	if tagString != tag {
+		t.Errorf("The tag should be String, box value %x.")
+	}
+	(&maxPositiveBox).SetTag(TagNumber)
+	tag = maxPositiveBox.Tag()
+	if math.MaxInt32 != maxPositiveBox.ToNumber() {
+		t.Errorf("maxPositiveBox value should be %d", math.MaxInt32)
+	}
+}
+
+func TestPayload(t *testing.T) {
+	maxPositiveBox := NewNumber(math.MinInt32)
+	maxPositiveBox = maxPositiveBox.SetPayload(uint64(math.MaxInt32))
+
+}
+
+func TestIs(t *testing.T) {
+	{
+		trueBox := NewBool(true)
+		if true == trueBox.IsFloat64() {
+			t.Error("trueBox should not be a valid float64")
+		}
+		if false == trueBox.IsBool() {
+			t.Error("trueBox should be a valid bool box")
+		}
+	}
+	{
+		maxNegativeBox := NewNumber(math.MinInt32)
+		if false == maxNegativeBox.IsNumber() {
+			t.Error("maxNegativeBox box type should be number")
+		}
+		randomPositiveBox := NewNumber(12) // Or almost random
+		if false == randomPositiveBox.IsNumber() {
+			t.Error("randomPositiveBox box type should be number")
+		}
+	}
+	{
+		laLaLaLaObject := make(Object)
+		laLaLaLaObject["three"] = NewString("Once")
+		laLaLaLaObject["little"] = NewString("upon")
+		laLaLaLaObject["pigs"] = NewString("a time...")
+		laLaLaLaBox := NewObject(laLaLaLaObject)
+		if false == laLaLaLaBox.IsObject() {
+			t.Error("laLaLaLaObject box type should be object")
+		}
+	}
+	{
+		leetArray := Array{NewNumber(1), NewNumber(3), NewNumber(3), NewNumber(7)}
+		leetBox := NewArray(leetArray)
+		if false == leetBox.IsArray() {
+			t.Error("leetArray box type should be array")
+		}
+	}
+}
